12a4a58ae67de1991a57c457d95b8d91045778da
MartimBaptista <martimdsbaptista@gmail.com>
2022-10-21 15:44:24 +0100

    simple mains to test the tcp connection

---
+++ testing_stuff/test_client.c
@@ -0,0 +1,6 @@
+#include "network_client.h"
+#include "client_stub-private.h"
+
+main(int argc, char const *argv[]){
+    //TODO
+}
---
+++ testing_stuff/test_server.c
@@ -0,0 +1,5 @@
+#include "network_server.h"
+
+main(int argc, char const *argv[]){
+    //TODO
+}
