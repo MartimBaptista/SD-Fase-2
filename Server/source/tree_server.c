0b02ad2a5d8421fe31b2b76278388267d6b3feb0
MartimBaptista <martimdsbaptista@gmail.com>
2022-10-26 17:52:02 +0100

    should be about this, but check later

--- Server/source/tree_server.c
+++ Server/source/tree_server.c
@@ -1,3 +1,18 @@
-main(){
-    //TODO
+#include "network_server.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+    if (argc != 2){
+        printf("Uso: ./tree_server <porto_servidor>\n");
+        printf("Exemplo de uso: ./tree_server 12345\n");
+        return -1;
+    }
+
+    int listening_socket = atoi(argv[1]);
+
+    network_main_loop(listening_socket);
+
+    return 0;
 }
